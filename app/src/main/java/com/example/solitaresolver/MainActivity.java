d9d23d53a0dabe089140f4a2c095e854756b873b
Sebastian <s163526@student.dtu.dk>
2021-02-19 19:38:25 +0100

    OpenCV successfully installed

---
+++ app/src/main/java/com/example/solitaresolver/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.solitaresolver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+    static {
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV installed.");
+        }
+        else Log.d(TAG, "OpenCV not installed");
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
