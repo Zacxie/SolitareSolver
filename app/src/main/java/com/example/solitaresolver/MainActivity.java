0e93ad2e479a5a123a3c63c2f6f8c51f39b86c28
Sebastian <s163526@student.dtu.dk>
2021-02-19 22:30:12 +0100

    Basic live feed camera implemented.

--- app/src/main/java/com/example/solitaresolver/MainActivity.java
+++ app/src/main/java/com/example/solitaresolver/MainActivity.java
@@ -1,25 +1,109 @@
 package com.example.solitaresolver;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
-
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.Toast;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity {
-    private static String TAG = "MainActivity";
-    static {
-        if (OpenCVLoader.initDebug()) {
-            Log.d(TAG, "OpenCV installed.");
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private CameraBridgeViewBase _cameraBridgeViewBase;
+
+    private BaseLoaderCallback _baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    _cameraBridgeViewBase.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+            }
         }
-        else Log.d(TAG, "OpenCV not installed");
-    }
-
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
+
+        // Permissions for Android 6+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.CAMERA},
+                1);
+
+        _cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.main_surface);
+        _cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        _cameraBridgeViewBase.setCvCameraViewListener(this);
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableCamera();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, _baseLoaderCallback);
+        } else {
+            _baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted.
+                } else {
+                    // permission denied.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        disableCamera();
+    }
+
+    public void disableCamera() {
+        if (_cameraBridgeViewBase != null)
+            _cameraBridgeViewBase.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat matGray = inputFrame.gray();
+        return matGray;
+    }
+
 }
