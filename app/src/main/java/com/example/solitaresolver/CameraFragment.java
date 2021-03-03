cd1ae00fad2f75085452c9f98e6a217f8667935f
Sebastian <s163526@student.dtu.dk>
2021-03-03 09:42:30 +0100

    Refactored the code from Main to CameraFragment

--- app/src/main/java/com/example/solitaresolver/CameraFragment.java
+++ app/src/main/java/com/example/solitaresolver/CameraFragment.java
@@ -1,64 +1,193 @@
 package com.example.solitaresolver;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CameraFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class CameraFragment extends Fragment {
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    public CameraFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CameraFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CameraFragment newInstance(String param1, String param2) {
-        CameraFragment fragment = new CameraFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class CameraFragment extends Fragment implements View.OnClickListener {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final int pic_id = 123;
+    String currentPhotoPath;
+
+    public Bitmap edgeDetectionBitmap;
+    private Button edgeDetectionButton, openCameraButton;
+    private ImageView imageView;
+    Bitmap photo;
+    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+        View root = inflater.inflate(R.layout.fragment_camera, container, false);
+
+        imageView = root.findViewById(R.id.imageView4);
+
+        openCameraButton = root.findViewById(R.id.openCameraButton);
+        edgeDetectionButton = root.findViewById(R.id.button2);
+        edgeDetectionButton.setOnClickListener(this);
+
+        openCameraButton.setOnClickListener(this);
+        OpenCVLoader.initDebug();
+
+        // Permissions for Android 6+
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{Manifest.permission.CAMERA},
+                1);
+
+        return root;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted.
+                } else {
+                    // permission denied.
+                    Toast.makeText(getActivity(), "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == pic_id) {
+            int targetW = imageView.getWidth();
+            int targetH = imageView.getHeight();
+
+            // Get the dimensions of the bitmap
+
+            bmOptions.inJustDecodeBounds = true;
+
+            BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+
+            int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
+
+            // Determine how much to scale down the image
+            int scaleFactor = Math.max(1, Math.min(photoW / targetW, photoH / targetH));
+
+            // Decode the image file into a Bitmap sized to fill the View
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+            bmOptions.inPurgeable = true;
+
+            photo = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+            imageView.setImageBitmap(photo);
+
+        }
+    }
+
+
+    public void edgeDetection(View v) {
+        Mat Rgba = new Mat();
+        Mat grayMat = new Mat();
+
+
+        bmOptions.inDither = false;
+        bmOptions.inSampleSize = 4;
+
+        int width = photo.getWidth();
+        int height = photo.getHeight();
+
+        edgeDetectionBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+
+        //Edge detection
+        Utils.bitmapToMat(photo, Rgba);
+        Imgproc.cvtColor(Rgba, grayMat, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.Canny(Rgba, grayMat, 100, 80);
+
+        Utils.matToBitmap(grayMat, edgeDetectionBitmap);
+
+        imageView.setImageBitmap(edgeDetectionBitmap);
+    }
+
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getContext(),
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, pic_id);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == edgeDetectionButton) {
+            edgeDetection(v);
+        }
+        if (v == openCameraButton) {
+            dispatchTakePictureIntent();
+
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/solitaresolver/MainActivity.java
+++ app/src/main/java/com/example/solitaresolver/MainActivity.java
@@ -1,45 +1,14 @@
 package com.example.solitaresolver;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.FileProvider;
+import androidx.fragment.app.FragmentManager;
 
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
+public class MainActivity extends AppCompatActivity  {
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private static final int pic_id = 123;
-    String currentPhotoPath;
-
-    public Bitmap edgeDetectionBitmap;
-    private Button edgeDetectionButton, openCameraButton;
-    private ImageView imageView;
-    Bitmap photo;
-    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
 
 
     @Override
@@ -47,152 +16,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.imageView4);
-
-        openCameraButton = findViewById(R.id.openCameraButton);
-        edgeDetectionButton = findViewById(R.id.button2);
-        edgeDetectionButton.setOnClickListener(this);
-
-        openCameraButton.setOnClickListener(this);
-        OpenCVLoader.initDebug();
-
-
-        // Permissions for Android 6+
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.CAMERA},
-                1);
-
-    }
-
-
-    @Override
-    public void onResume() {
-        super.onResume();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.activity_frame_layout, new CameraFragment())
+                .commit();
 
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted.
-                } else {
-                    // permission denied.
-                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == pic_id) {
-            int targetW = imageView.getWidth();
-            int targetH = imageView.getHeight();
-
-            // Get the dimensions of the bitmap
-
-            bmOptions.inJustDecodeBounds = true;
-
-            BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
-
-            int photoW = bmOptions.outWidth;
-            int photoH = bmOptions.outHeight;
-
-            // Determine how much to scale down the image
-            int scaleFactor = Math.max(1, Math.min(photoW / targetW, photoH / targetH));
-
-            // Decode the image file into a Bitmap sized to fill the View
-            bmOptions.inJustDecodeBounds = false;
-            bmOptions.inSampleSize = scaleFactor;
-            bmOptions.inPurgeable = true;
-
-            photo = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
-            imageView.setImageBitmap(photo);
-
-        }
-    }
-
-    public void edgeDetection(View v) {
-        Mat Rgba = new Mat();
-        Mat grayMat = new Mat();
 
 
-        bmOptions.inDither = false;
-        bmOptions.inSampleSize = 4;
-
-        int width = photo.getWidth();
-        int height = photo.getHeight();
-
-        edgeDetectionBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-
-        //Edge detection
-        Utils.bitmapToMat(photo, Rgba);
-        Imgproc.cvtColor(Rgba, grayMat, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.Canny(Rgba, grayMat, 100, 80);
-
-        Utils.matToBitmap(grayMat, edgeDetectionBitmap);
-
-        imageView.setImageBitmap(edgeDetectionBitmap);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v == edgeDetectionButton) {
-            edgeDetection(v);
-        }
-        if (v == openCameraButton) {
-
-            dispatchTakePictureIntent();
 
 
-        }
-    }
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
 
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
 
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, pic_id);
-            }
-        }
-    }
+
+
+
+
+
 
 
 }
