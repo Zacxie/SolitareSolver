38e62e362856ce30d6c8908e47b611b463592f22
Marie <S185363@student.dtu.dk>
2021-03-03 11:36:24 +0100

    Menu tilføjet

--- app/src/main/java/com/example/solitaresolver/Mainmenu.java
+++ app/src/main/java/com/example/solitaresolver/Mainmenu.java
@@ -2,63 +2,91 @@ package com.example.solitaresolver;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Mainmenu#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Mainmenu extends Fragment {
+public class Mainmenu extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button cameraButton, settingsButton, helpButton;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Mainmenu() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Hovedmenu.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Mainmenu newInstance(String param1, String param2) {
+    public static Mainmenu newInstance() {
         Mainmenu fragment = new Mainmenu();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_mainmenu, container, false);
+
+        cameraButton = v.findViewById(R.id.mainCamera);
+        settingsButton =v.findViewById(R.id.mainSettings);
+        helpButton = v.findViewById(R.id.mainHelp);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mainmenu, container, false);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == cameraButton){
+            //todo start camera action
+
+
+        }
+
+        else if (v == settingsButton){
+
+            System.out.println("klikkede på to settings");
+
+            FragmentManager fragmentManager2 = getChildFragmentManager();
+            FragmentTransaction fragmentTransaction2 = fragmentManager2.beginTransaction();
+
+            Settings settings = new Settings();
+            fragmentTransaction2.add(R.id.content_from_buttons, settings); //content_from_buttons er det område som fragmentet skal laves i.
+
+            fragmentTransaction2.commit();
+
+        }else if (v == helpButton){
+            System.out.println("klikkede på to settings");
+
+            FragmentManager fragmentManager3 = getChildFragmentManager();
+            FragmentTransaction fragmentTransaction3 = fragmentManager3.beginTransaction();
+
+//            HelpFragment helpFragment = new Settings(); //todo opret help fragment
+//            fragmentTransaction3.add(R.id.mainActFillFragment, settings); //mainActFillFragment er det område som fragmentet skal laves i.
+
+            fragmentTransaction3.commit();
+
+        }
+
+
+
+
     }
 }
