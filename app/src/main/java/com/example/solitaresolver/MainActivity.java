7548aa4514e1c8676008831d87968045b440090a
Sebastian <s163526@student.dtu.dk>
2021-03-24 12:15:08 +0100

    Image recon working. Showing rectangle slightly below object. Needs fixing

--- app/src/main/java/com/example/solitaresolver/MainActivity.java
+++ app/src/main/java/com/example/solitaresolver/MainActivity.java
@@ -16,12 +16,23 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfRect2d;
+import org.opencv.core.Point;
+import org.opencv.core.Rect2d;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.dnn.Dnn;
 import org.opencv.dnn.Net;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -35,9 +46,9 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     public void YOLO(View Button) {
         if (startYolo == false) {
             startYolo = true;
-            if (firstTimeYolo ==false) {
-                String tinyYoloCfg = Environment.getExternalStorageDirectory()+"/dnns/yolov3-tiny.cfg";
-                String tinyYoloWeights = Environment.getExternalStorageDirectory()+"/dnns/yolov3-tiny.weights";
+            if (firstTimeYolo == false) {
+                String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg";
+                String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
                 tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
             }
         } else {
@@ -131,15 +142,88 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
         if (startYolo == true) {
             Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
-            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392,  new Size(416,416), new Scalar(0,0,0), false);
+            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416, 416), new Scalar(0, 0, 0), false);
             tinyYolo.setInput(imageBlob);
             tinyYolo.forward();
 
 
+            List<Mat> result = new ArrayList<Mat>(2);
+            List<String> outBlobNames = new ArrayList<>();
+            outBlobNames.add(0, "yolo_16");
+            outBlobNames.add(1, "yolo_23");
 
-            //Edge detection
-            //Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGB2GRAY);
-            //Imgproc.Canny(frame, frame, 100, 80);
+            tinyYolo.forward(result, outBlobNames);
+
+            float confThreshold = 0.3f;
+
+            List<Integer> clsIds = new ArrayList<>();
+            List<Float> confs = new ArrayList<>();
+            List<Rect2d> rects = new ArrayList<>();
+
+            for (int i = 0; i < result.size(); i++) {
+
+                Mat level = result.get(i);
+                for (int j = 0; j < level.rows(); j++) {
+                    Mat row = level.row(j);
+                    Mat scores = row.colRange(5, level.cols());
+                    Core.MinMaxLocResult mm = Core.minMaxLoc(scores);
+
+                    float confidence = (float) mm.maxVal;
+
+                    Point classIdPoint = mm.maxLoc;
+
+                    if (confidence > confThreshold) {
+
+                        int centerX = (int) (row.get(0, 0)[0] * frame.cols());
+                        int centerY = (int) (row.get(0, 1)[0] * frame.cols());
+                        int width = (int) (row.get(0, 2)[0] * frame.cols());
+                        int height = (int) (row.get(0, 3)[0] * frame.cols());
+                        int left = centerX - width / 2;
+                        int top = centerY - height / 2;
+
+                        clsIds.add((int) classIdPoint.x);
+                        confs.add((float) confidence);
+                        rects.add(new Rect2d(left, top, width, height));
+                    }
+                }
+            }
+            int ArrayLength = confs.size();
+            if (ArrayLength >=1) {
+                float nmsThresh = 0.2f;
+                MatOfFloat confidences = new MatOfFloat(Converters.vector_float_to_Mat(confs));
+                Rect2d[] boxesArray = rects.toArray(new Rect2d[0]);
+                MatOfRect2d boxes = new MatOfRect2d(boxesArray);
+                MatOfInt indices = new MatOfInt();
+
+                Dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThresh, indices);
+
+                int[] ind = indices.toArray();
+                for (int i = 0; i < ind.length; ++i) {
+
+                    int idx = ind[i];
+                    Rect2d box = boxesArray[idx];
+
+                    int idGuy = clsIds.get(idx);
+
+                    float conf = confs.get(idx);
+
+
+                    List<String> cocoNames = Arrays.asList("a person", "a bicycle", "a motorbike", "an airplane", "a bus", "a train", "a truck", "a boat", "a traffic light", "a fire hydrant", "a stop sign", "a parking meter", "a car", "a bench", "a bird", "a cat", "a dog", "a horse", "a sheep", "a cow", "an elephant", "a bear", "a zebra", "a giraffe", "a backpack", "an umbrella", "a handbag", "a tie", "a suitcase", "a frisbee", "skis", "a snowboard", "a sports ball", "a kite", "a baseball bat", "a baseball glove", "a skateboard", "a surfboard", "a tennis racket", "a bottle", "a wine glass", "a cup", "a fork", "a knife", "a spoon", "a bowl", "a banana", "an apple", "a sandwich", "an orange", "broccoli", "a carrot", "a hot dog", "a pizza", "a doughnut", "a cake", "a chair", "a sofa", "a potted plant", "a bed", "a dining table", "a toilet", "a TV monitor", "a laptop", "a computer mouse", "a remote control", "a keyboard", "a cell phone", "a microwave", "an oven", "a toaster", "a sink", "a refrigerator", "a book", "a clock", "a vase", "a pair of scissors", "a teddy bear", "a hair drier", "a toothbrush");
+
+
+
+                    int intConf = (int) (conf * 100);
+
+
+
+                    Imgproc.putText(frame,cocoNames.get(idGuy) + " " + intConf + "%",box.tl(),Core.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255,255,0),2);
+
+
+
+                    Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(255, 0, 0), 2);
+
+                }
+            }
 
         }
 
